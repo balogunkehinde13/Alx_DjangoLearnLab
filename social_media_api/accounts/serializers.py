7af7384a6b4fd82bb52ad0c37a8c883a80912728
balogunkehinde13 <balogunraheem@gmail.com>
2025-12-13 18:34:09 +0100

    fised serializers

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,18 +2,16 @@ from django.contrib.auth import authenticate, get_user_model
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 
-User = get_user_model()
-
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ['username', 'email', 'password', 'bio']
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email'),
             password=validated_data['password'],
@@ -35,6 +33,7 @@ class LoginSerializer(serializers.Serializer):
         )
         if not user:
             raise serializers.ValidationError("Invalid credentials")
+
         return user
 
 
@@ -43,7 +42,7 @@ class UserProfileSerializer(serializers.ModelSerializer):
     following_count = serializers.IntegerField(source='following.count', read_only=True)
 
     class Meta:
-        model = User
+        model = get_user_model()
         fields = [
             'username',
             'email',
